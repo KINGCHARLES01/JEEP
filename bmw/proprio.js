143bb4d91756b2b363a6105112ab618265bf7825
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-22 14:47:07 +0300

    proprio.js

--- bmw/proprio.js
+++ bmw/proprio.js
@@ -14,7 +14,7 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("This command is for owner only");
+    return repondre("This command is for Marisel only");
   }
 
   const {exec}=require("child_process")
